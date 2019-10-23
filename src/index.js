20c48db617a5a5a8872bc2db8a5ea699b809afcc
Константин <arkanyga96@gmail.com>
2019-10-23 21:05:14 +0300

    'компоненты'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build awesome app</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>My todo List</h1>;
+};
+
+const SearchPanel =()=>{
+    return <input placeholder='search'/>;
+};
+
+const App = () =>{
+  return (
+      <div>
+          <AppHeader/>
+          <SearchPanel/>
+          <TodoList/>
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
